8cbd06efd6ce41f2193c28f457ade58eaed852b5
bayron in vmdocker01 <bayronq@vmdocker01.bayronq.com>
2025-05-10 12:03:06 -0600

    se agregó script sql

--- sqlserver/sqlserver.sh
+++ sqlserver/sqlserver.sh
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Admin01." -e "MSSQL_PID=Express" -p 1433:1433 --name sqlserver -d mcr.microsoft.com/mssql/server:2022-latest -v /work/sqlserver-data:/var/opt/mssql
 =======
 docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Admin01." -e "MSSQL_PID=Express" -p 1433:1433 --name sqlserver -d mcr.microsoft.com/mssql/server:2019-latest 
 >>>>>>> b48b0c8 (update)
+=======
+docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Admin01." -e "MSSQL_PID=Express" -p 1433:1433 --name sqlserver -d mcr.microsoft.com/mssql/server:2022-latest -v /work/sqlserver-data:/var/opt/mssql
+>>>>>>> 4f7b844 (se actualizó el compose de sqlserver agregarndo volumes en /work/volumes/sqlserver-data)
